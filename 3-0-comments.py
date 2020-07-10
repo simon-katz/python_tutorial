3bc177ed30f259ccba8f98083796581b66cb4963
Simon Katz <sk@nomistech.com>
2020-07-10 20:16:27 +0100

    3-0-comments.py

---
+++ 3-0-comments.py
@@ -0,0 +1,8 @@
+# Copied from https://docs.python.org/3/tutorial/introduction.html
+
+spam = 1  # and this is the second comment
+          # ... and now a third!
+text = "# This is not a comment because it's inside quotes."
+
+print(spam)
+print(text)
